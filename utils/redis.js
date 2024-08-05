239032b8d818c99fe51ff4dad57ca8123ccf9037
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-08-05 11:51:40 +0300

    Task 0

--- utils/redis.js
+++ utils/redis.js
@@ -1,6 +1,7 @@
 import { createClient } from 'redis';
 import { promisify } from 'util';
 
+// Class defining methods for commonly used Redis commands
 class RedisClient {
   constructor() {
     this.client = createClient();
@@ -9,22 +10,26 @@ class RedisClient {
     });
   }
 
+  // Check if the Redis client is connected
   isAlive() {
     return this.client.connected;
   }
 
+  // Get the value for a given key from Redis server
   async get(key) {
     const redisGet = promisify(this.client.get).bind(this.client);
     const value = await redisGet(key);
     return value;
   }
 
+  // Set a key-value pair in Redis server with expiration time
   async set(key, value, time) {
     const redisSet = promisify(this.client.set).bind(this.client);
     await redisSet(key, value);
     await promisify(this.client.expire).bind(this.client)(key, time);
   }
 
+  // Delete a key-value pair from Redis server
   async del(key) {
     const redisDel = promisify(this.client.del).bind(this.client);
     await redisDel(key);
@@ -34,4 +39,3 @@ class RedisClient {
 const redisClient = new RedisClient();
 
 export default redisClient;
-
