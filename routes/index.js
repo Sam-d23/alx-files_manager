c61c3437bb7626affff95263b0e7637c98d3036c
Sam-d23 <123358241+Sam-d23@users.noreply.github.com>
2024-08-07 23:06:38 +0300

    Files Manager

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,28 @@ import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
-router
-  .get('/status', AppController.getStatus)
-  .get('/stats', AppController.getStats)
-  .post('/users', UsersController.postNew)
-  .get('/connect', AuthController.getConnect)
-  .get('/disconnect', AuthController.getDisconnect)
-  .get('/users/me', UsersController.getMe)
-  .post('/files', FilesController.postUpload)
-  .get('/files/:id', FilesController.getShow)
-  .get('/files', FilesController.getIndex)
-  .put('/files/:id/publish', FilesController.putPublish)
-  .put('/files/:id/unpublish', FilesController.putUnpublish)
-  .get('/files/:id/data', FilesController.getFile);
+router.get('/status', AppController.getStatus);
+
+router.get('/stats', AppController.getStats);
+
+router.post('/users', UsersController.postNew);
+
+router.get('/connect', AuthController.getConnect);
+
+router.get('/disconnect', AuthController.getDisconnect);
+
+router.get('/users/me', UsersController.getMe);
+
+router.post('/files', FilesController.postUpload);
+
+router.get('/files/:id', FilesController.getShow);
+
+router.get('/files', FilesController.getIndex);
+
+router.put('/files/:id/publish', FilesController.putPublish);
+
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
